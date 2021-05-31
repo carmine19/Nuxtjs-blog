8c92e413b84b2d6658f74b4495754d4ae1cb8dee
carmine <carminepepiciello@gmail.com>
2021-05-31 16:13:34 +0200

    pagina post con parametro id

---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>singolo post</h1>
+
+      post : {{$route.params.id}}
+
+  </div>
+</template>
+
+
+
+
+
+
+<script>
+export default {
+
+}
+</script>
+
+
+
+
+<style type="scss">
+
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>pagina post</h1>
+  </div>
+</template>
+
+
+
+
+
+
+<script>
+export default {
+
+}
+</script>
+
+
+
+
+<style type="scss">
+
+</style>
