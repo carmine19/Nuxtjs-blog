43c4320b7b8a987b92fc778517e554b7a92135ec
carmine <carminepepiciello@gmail.com>
2021-06-04 14:56:10 +0200

    Creazione componente FormPost

---
+++ components/FormPost.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <form>
+      <div class="mb-3">
+        <label for="id" class="form-label">{{newPost.id}}</label>
+        <input v-model="newPost.id" class="form-control" type="text" placeholder="id">
+      </div>
+      <div class="mb-3">
+        <label for="title" class="form-label">{{newPost.title}}</label>
+        <input v-model="newPost.title" class="form-control" type="text" placeholder="title">
+      </div>
+
+      <div class="mb-3">
+        <label for="description" class="form-label">{{newPost.description}}</label>
+        <input v-model="newPost.description" class="form-control" type="text" placeholder="description">
+      </div>
+
+      <div class="mb-3">
+        <label for="cover" class="form-label">{{newPost.cover}}</label>
+        <input v-model="newPost.cover" class="form-control" type="text" placeholder="cover">
+      </div>
+
+
+      <button @click="save" type="" class="btn btn-primary">Submit</button>
+
+      <button @click="cancel" type="reset" class="btn btn-primary">Cancel</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormPost",
+
+  data() {
+    return {
+      newPost : {
+        id : '',
+        title : '',
+        description : '',
+        cover: '',
+      }
+    }
+  },
+
+  methods : {
+    save() {
+      console.log(this.newPost)
+    },
+    cancel() {
+      this.$router.push('/admin')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>new-post</h1>
+    <FormPost  />
   </div>
 </template>
 
