787a2259ceb80eb06e38064268d12e0d2ff4e03e
carmine <carminepepiciello@gmail.com>
2021-06-04 16:57:31 +0200

    Aggiunto template e rotta edit per post

--- components/Card.vue
+++ components/Card.vue
@@ -9,6 +9,7 @@
                che il tag e di tipo 'a' e poi passiamo l'id come parametro legato a post, ricordati che
                precedentemente hai già definito nella cartella post sia la rotta priincipale che l'id-->
           <NuxtLink tag="a" :to=" /posts/ + this.id" class="btn btn-primary">Post numero: {{id}}</NuxtLink>
+          <button v-if="" @click="linkToEdit(id)" class="btn btn-danger">edit</button>
         </div>
       </div>
     </div>
@@ -21,6 +22,12 @@ export default {
   data() {
     return {
 
+    }
+  },
+
+  methods : {
+    linkToEdit(id) {
+      this.$router.push('/admin/new-post/' + id)
     }
   },
 
@@ -42,6 +49,10 @@ export default {
     cover : {
       type : String,
       required : true
+    },
+    isAdmin : {
+      type : Boolean,
+      default : false
     }
   }
 }
---
+++ pages/admin/new-post/_postid/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    post edit
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
